08cde97bec61020a7fdd95abf0b5781563e0052f
Исупов Максим Олегович <144238218+maksincheks@users.noreply.github.com>
2023-12-07 16:02:51 +0300

    Add files via upload

---
+++ Задание 10_2.pas
@@ -0,0 +1,19 @@
+﻿﻿Program Zad_10_2; 
+var i,j : integer;
+A : array [1..8,1..6] of integer;
+B : array [1..8] of integer;
+begin
+  for i := 1 to 8 do begin
+    for j := 1 to 6 do begin
+      readln(A[i,j]);
+    end;
+    for j := 1 to 6 do begin
+      B[i] := 1;
+      if A[i,j] < 0 then begin
+        B[i] := -1;
+        break;
+      end;
+    end;
+  end;
+  writeln(B);
+end.
